d74b3d150e23427deee3a62297f6ecc0f962f9ca
Donattai <donattai1032@gmail.com>
2020-08-04 07:43:31 +0100

    removed login_required

--- tasks/views.py
+++ tasks/views.py
@@ -1,11 +1,9 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.decorators import login_required
 
 from .models import *
 from .forms import TodoForm
 
 # Create your views here.
-@login_required(login_url='/')
 def index(request):
     todos = Todo.objects.all()
     form = TodoForm
