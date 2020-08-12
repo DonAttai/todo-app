6a24ccfe0e4cb7f8df6a4030d507c91f6d7454d4
Donattai <donattai1032@gmail.com>
2020-08-12 01:26:31 +0100

    modified todos/urls

--- todos/urls.py
+++ todos/urls.py
@@ -15,10 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.contrib.auth import views as auth_views
+from users.views import register
+
 
 urlpatterns = [
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path('register/', register, name='register'),
     path('', include('tasks.urls')),
-    path('users/', include('users.urls')),
     path('admin/', admin.site.urls),
     
 
