3913a52f964bba57ae605b5bdf79efa4747c38e9
Donattai <donattai1032@gmail.com>
2020-10-28 09:13:13 +0100

    modified the navbar

--- todos/settings.py
+++ todos/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'c)e+(p72y6@k-ws!pq54h68$w(7$7e%-t86hs#%9gym&ps!38u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # ALLOWED_HOSTS = ['johntodoapp.herokuapp.com', '127.0.0.1']
 
